7cb5ef7b93ba06dc891716afa698419891ed5445
morkolai <nikolai-mork@live.no>
2021-12-22 13:02:50 +0100

    2020 day 2 part 2

--- 2021/day2.dart
+++ 2021/day2.dart
@@ -2,23 +2,49 @@ import 'dart:io';
 
 void main() {
   var instructions = File('input/input_day2').readAsLinesSync();
-  print(partOne(instructions));
+  print("Part one: ${partOne(instructions)}");
+  print("Part two: ${partTwo(instructions)}");
 }
 
 int partOne(List<String> instructions) {
   var x = 0, y = 0;
+
   for (String instruction in instructions) {
-    var temp = instruction.split(" ");
-    switch (temp[0]) {
+    var text = instruction.split(" ")[0],
+        number = int.parse(instruction.split(" ")[1]);
+
+    switch (text) {
       case 'forward':
-        x += int.parse(temp[1]);
+        x += number;
         break;
       case 'up':
-        y -= int.parse(temp[1]);
+        y -= number;
         break;
       case 'down':
-        y += int.parse(temp[1]);
+        y += number;
         break;
+    }
+  }
+  return x * y;
+}
+
+int partTwo(List<String> instructions) {
+  int x = 0, y = 0, aim = 0;
+
+  for (String instruction in instructions) {
+    var text = instruction.split(" ")[0],
+        number = int.parse(instruction.split(" ")[1]);
+
+    switch (text) {
+      case "down":
+        aim += number;
+        break;
+      case "up":
+        aim -= number;
+        break;
+      case "forward":
+        x += number;
+        y += aim * number;
     }
   }
   return x * y;
